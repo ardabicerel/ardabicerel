fe7da29b0ce6e1615645440543f8528eafce8817
Arda BİÇEREL <149125335+ardabicerel@users.noreply.github.com>
2023-12-30 15:24:56 +0300

    Add files via upload

--- Taş-Kağıt-Makas Oyunu/taskagitmakas.c
+++ Taş-Kağıt-Makas Oyunu/taskagitmakas.c
@@ -10,88 +10,191 @@ int main() {
 	int oyuncu_puan=0;
 	int oyun_sayisi;
 	int sayac=1;
+	int dil_secim;
 	
-	printf("----------Tas Kagit Makas----------\n\n");
+	printf("-----------------------------------\n\n");
+	printf("1-Turkish\n2-English\n\n");
+	printf("Oynamak icin dilinizi giriniz(Choose your language for play) : ");
+	scanf("%d",&dil_secim);
 	
-	printf("Kac oyun oynanacagini giriniz : ");
-	scanf("%d",&oyun_sayisi);
+	system("cls");
 	
-	while(sayac<=oyun_sayisi)
+	if(dil_secim==1)
 	{
+		printf("----------Tas Kagit Makas----------\n\n");
+	
+		printf("Kac oyun oynanacagini giriniz : ");
+		scanf("%d",&oyun_sayisi);
+	
+		while(sayac<=oyun_sayisi)
+		{
 		
 				
-		printf("\n\n0-Tas\n1-Kagit\n2-Makas\n\nSeciminizi giriniz : ");
-		scanf("%d",&oyuncu_secim);
+			printf("\n\n0-Tas\n1-Kagit\n2-Makas\n\nSeciminizi giriniz : ");
+			scanf("%d",&oyuncu_secim);
 		
-		srand(time(NULL));
-		bilgisayar_secim = rand() % 3; //0,1,2 sayýlarý arasýnda sayý oluþturur.
+			srand(time(NULL));
+			bilgisayar_secim = rand() % 3; //0,1,2 sayýlarý arasýnda sayý oluþturur.
 		
-		switch(oyuncu_secim)
-		{
-		case 0:
-			printf("\nTas-");
-			break;
-	
-		case 1:
-			printf("\nKagit-");
-			break;
-	
-		case 2:
-			printf("\nMakas-");
-			break;
-		
-		default:
-			printf("\nYanlis deger girildi. Oyun bitiriliyor.\n\n");
-			break;
-		}
-		
-		switch(bilgisayar_secim)
-		{
+			switch(oyuncu_secim)
+			{
 			case 0:
-				printf("Tas\n");
+				printf("\nTas-");
 				break;
-			
+	
 			case 1:
-				printf("Kagit\n");
+				printf("\nKagit-");
 				break;
-				
+	
 			case 2:
-				printf("Makas\n");
+				printf("\nMakas-");
 				break;
-		}
 		
-		if(bilgisayar_secim == 0 && oyuncu_secim == 2 || bilgisayar_secim == 1 && oyuncu_secim == 0 || bilgisayar_secim == 2 && oyuncu_secim == 1)
+			default:
+				printf("\nYanlis deger girildi. Oyun bitiriliyor.\n\n");
+				break;
+			}
+		
+			switch(bilgisayar_secim)
+			{
+				case 0:
+					printf("Tas\n\n");
+					break;
+			
+				case 1:
+					printf("Kagit\n\n");
+					break;
+				
+				case 2:
+					printf("Makas\n\n");
+					break;
+			}
+		
+			if(bilgisayar_secim == 0 && oyuncu_secim == 2 || bilgisayar_secim == 1 && oyuncu_secim == 0 || bilgisayar_secim == 2 && oyuncu_secim == 1)
+			{
+				bilgisayar_puan++;
+			}
+		
+			else if(oyuncu_secim == 0 && bilgisayar_secim == 2 || oyuncu_secim == 1 && bilgisayar_secim == 0 || oyuncu_secim == 2 && bilgisayar_secim == 1)
+			{
+				oyuncu_puan++;
+			}
+		
+			printf("Bilgisayar Puan : %d\nOyuncu Puan : %d",bilgisayar_puan,oyuncu_puan);
+		
+			printf("\n\n-----------------------------------");
+	
+			sayac++;
+		}
+	
+		if(bilgisayar_puan > oyuncu_puan)
 		{
-			bilgisayar_puan++;
+			printf("\n\n***Bilgisayar kazandi***\n");
 		}
-		
-		else if(oyuncu_secim == 0 && bilgisayar_secim == 2 || oyuncu_secim == 1 && bilgisayar_secim == 0 || oyuncu_secim == 2 && bilgisayar_secim == 1)
+	
+		else if(oyuncu_puan > bilgisayar_puan)
 		{
-			oyuncu_puan++;
+			printf("\n\n***Oyuncu1 oyunu kazandi***\n");
 		}
+	
+		else if(oyuncu_puan == bilgisayar_puan)
+		{
+			printf("\n\nBu oyunlar sonucunda kazanan olmadi.\n");
+		}
+	}
+	
+	else if(dil_secim==2)
+	{
+		printf("----------Rock Paper Scissors----------\n\n");
+	
+		printf("Please enter how many round will you play : ");
+		scanf("%d",&oyun_sayisi);
+	
+		while(sayac<=oyun_sayisi)
+		{
 		
-		printf("Bilgisayar : %d\nOyuncu Puan : %d",bilgisayar_puan,oyuncu_puan);
+				
+			printf("\n\n0-Rock\n1-Paper\n2-Scissors\n\nEnter your choice : ");
+			scanf("%d",&oyuncu_secim);
 		
+			srand(time(NULL));
+			bilgisayar_secim = rand() % 3; //0,1,2 sayýlarý arasýnda sayý oluþturur.
+		
+			switch(oyuncu_secim)
+			{
+			case 0:
+				printf("\nRock-");
+				break;
+	
+			case 1:
+				printf("\nPaper-");
+				break;
+	
+			case 2:
+				printf("\nScissors-");
+				break;
+		
+			default:
+				printf("\nWrong value has been entered. The game is finished.\n\n");
+				break;
+			}
+		
+			switch(bilgisayar_secim)
+			{
+				case 0:
+					printf("Rock\n\n");
+					break;
+			
+				case 1:
+					printf("Paper\n\n");
+					break;
+				
+				case 2:
+					printf("Scissors\n\n");
+					break;
+			}
+		
+			if(bilgisayar_secim == 0 && oyuncu_secim == 2 || bilgisayar_secim == 1 && oyuncu_secim == 0 || bilgisayar_secim == 2 && oyuncu_secim == 1)
+			{
+				bilgisayar_puan++;
+			}
+		
+			else if(oyuncu_secim == 0 && bilgisayar_secim == 2 || oyuncu_secim == 1 && bilgisayar_secim == 0 || oyuncu_secim == 2 && bilgisayar_secim == 1)
+			{
+				oyuncu_puan++;
+			}
+		
+			printf("Computer Score : %d\nPlayer Score : %d",bilgisayar_puan,oyuncu_puan);
+		
+			printf("\n\n-----------------------------------");
+	
+			sayac++;
+		}
+	
+		if(bilgisayar_puan > oyuncu_puan)
+		{
+			printf("\n\n***Computer won the game.***\n");
+		}
+	
+		else if(oyuncu_puan > bilgisayar_puan)
+		{
+			printf("\n\n***Player1 won the game.***\n");
+		}
+	
+		else if(oyuncu_puan == bilgisayar_puan)
+		{
+			printf("\n\nAt the end of the rounds, there is no winner.\n");
+		}
+	}
+	
+	else
+	{
+		printf("-----------------------------------\n\n");
+		printf("Wrong choice. The game is finished.");
 		printf("\n\n-----------------------------------");
-	
-		sayac++;
+		exit(1);
 	}
 	
-	if(bilgisayar_puan > oyuncu_puan)
-	{
-		printf("\n\n***Bilgisayar kazandi***\n");
-	}
-	
-	else if(oyuncu_puan > bilgisayar_puan)
-	{
-		printf("\n\n***Oyuncu1 oyunu kazandi***\n");
-	}
-	
-	else if(oyuncu_puan == bilgisayar_puan)
-	{
-		printf("\n\nBu oyunlar sonucunda kazanan olmadi.\n");
-	}
-    
     return 0;
 }
 
